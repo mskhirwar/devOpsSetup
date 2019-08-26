9853a2f79455fdf24331b94baaef6bcc2014c095
nishantkhandelwal <nishant002@gmail.com>
2019-08-26 07:40:15 +0000

    changes for dockerfile

---
+++ install_jenkins.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# this script is only tested on ubuntu xenial
+
+# install docker
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+apt-get update
+apt-get install -y docker-engine
+systemctl enable docker
+systemctl start docker
+usermod -aG docker ubuntu
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# run jenkins
+mkdir -p /var/jenkins_home
+chown -R 1000:1000 /var/jenkins_home/
+docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -d --name jenkins nishant002/jenkins-with-docker:v1
+
+# show endpoint
+echo 'Jenkins installed'
+echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'
